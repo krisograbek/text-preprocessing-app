3295bf8256007cfcb31876d42ddc655108062fe4
krisograbek <krzysztof.ograbek@gmail.com>
2021-12-15 12:11:31 +0100

    allow copying to clipboard, display info

--- src/components/OutputText.js
+++ src/components/OutputText.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import TextField from '@mui/material/TextField'
+import React from 'react';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 
 import makeStyles from '@mui/styles/makeStyles';
@@ -11,15 +15,22 @@ const useStyles = makeStyles((theme) => ({
     },
     '& textarea.Mui-disabled': {
       // color: 'white',
-      textFillColor: 'white'
+      textFillColor: 'white',
+      '&:hover': {
+        cursor: "pointer"
+      },
+      // '& tooltip:hover': {
+      //   cursor: "pointer"
+      // },
     },
     '& .MuiOutlinedInput-root': {
       '&:hover fieldset': {
         borderColor: theme.palette.primary.light,
       },
-      // '& fieldset': {
+      '& fieldset': {
+        // cursor: ,
+      }
     }
-    // }
   },
 }))
 
@@ -44,26 +55,65 @@ const CssTextField = styled(TextField)({
   },
 });
 
-function InputText(props) {
-  const classes = useStyles()
-  const { outputText, setOutputText } = props
+// Man, I had to move this outside of the function to be 
+// able to reset timer
+let timer;
+
+function OutputText(props) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const { outputText, setOutputText } = props;
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = (event) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    setAnchorEl(event.currentTarget);
+    navigator.clipboard.writeText(outputText);
+    timer = setTimeout(() => setAnchorEl(null), 3000)
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
-    <TextField
-      id="fullWidth"
-      className={classes.textField}
-      multiline
-      fullWidth
-      disabled
-      rows={5}
-      label="Output"
-      value={outputText}
-      // onChange={e => setOutputText(e.target.value)}
-      inputProps={{
-        maxLength: 1000,
-      }}
-      sx={{ color: "green" }}
-    />
+    <div>
+      <Tooltip title="Copy to Clipboard">
+        <TextField
+          id="fullWidth"
+          className={classes.textField}
+          multiline
+          fullWidth
+          disabled
+          rows={5}
+          label="Output"
+          value={outputText}
+          // onChange={e => setOutputText(e.target.value)}
+          onClick={(event) => handleClick(event)}
+          inputProps={{
+            maxLength: 1000,
+          }}
+          sx={{ color: "green" }}
+        />
+      </Tooltip>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>Text Copied!</Typography>
+      </Popover>
+    </div>
   )
 }
-
-export default InputText
+export default OutputText
